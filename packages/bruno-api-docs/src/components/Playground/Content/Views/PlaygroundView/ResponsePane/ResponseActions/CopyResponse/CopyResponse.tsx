d8c7ae6953b5062b7a5bc5c639bbf57a2dce536c
Vineet Sharma (vasharma05) <vineet@usebruno.com>
2026-08-10 12:47:29 +0530

    fix: Match the response action icons to the Bruno app (BRU-3765) (#31)
    * Match the Action Icon buttons to that of the app
    
    * Increase the response actions expanded width to fix the collapsing width for response actions
    
    * Address comments
    
    * Remove the unused import

--- packages/bruno-api-docs/src/components/Playground/Content/Views/PlaygroundView/ResponsePane/ResponseActions/CopyResponse/CopyResponse.tsx
+++ packages/bruno-api-docs/src/components/Playground/Content/Views/PlaygroundView/ResponsePane/ResponseActions/CopyResponse/CopyResponse.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IconCheck, IconCopy } from '@tabler/icons';
-import IconButton from '@/ui/IconButton/IconButton';
+import ActionIconButton from '../ActionIconButton/ActionIconButton';
 
 interface CopyResponseProps {
   copied: boolean;
@@ -10,9 +10,13 @@ interface CopyResponseProps {
 
 const CopyResponse: React.FC<CopyResponseProps> = ({ copied, onClick, disabled }) => {
   return (
-    <IconButton label="Copy Response" className="p-1" disabled={disabled} onClick={onClick}>
-      {copied ? <IconCheck size={13} stroke={1.5} style={{ color: 'var(--text-muted)' }} /> : <IconCopy size={13} stroke={1.5} style={{ color: 'var(--text-muted)' }} />}
-    </IconButton>
+    <ActionIconButton label="Copy Response" className="p-1" disabled={disabled} onClick={onClick}>
+      {copied ? (
+        <IconCheck size={16} stroke={1.5} style={{ color: 'var(--text-muted)' }} />
+      ) : (
+        <IconCopy size={16} stroke={1.5} style={{ color: 'var(--text-muted)' }} />
+      )}
+    </ActionIconButton>
   );
 };
 

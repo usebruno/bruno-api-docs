eab43fee3b869ec728e41df4fb5f19aa6ed677b4
Sachin Venugopalan <sachin@usebruno.com>
2026-08-14 16:39:48 +0530

    Comments addressed

--- packages/bruno-api-docs/src/components/Playground/Content/Views/Common/ResponseBodyTab.tsx
+++ packages/bruno-api-docs/src/components/Playground/Content/Views/Common/ResponseBodyTab.tsx
@@ -28,7 +28,7 @@ const ResponseBodyTab: React.FC<ResponseBodyTabProps> = ({ response, selectedFor
   );
 
   return (
-    <div className="flex-1">
+    <div className="flex-1 min-h-0">
       {hideForLargeResponse ? (
         <LargeResponseWarning
           responseSize={responseSize}
